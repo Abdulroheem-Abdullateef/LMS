5c7f2c4279e22a66bc08fb04b108729692978efe
Abdulroheem-Abdullateef <roheemlateef.3@gmail.com>
2023-02-01 15:54:14 +0100

    commit

---
+++ Migrations/20220114152049_Solve.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearningManagementSystem.Migrations
+{
+    public partial class Solve : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Instructors_InstructorId",
+                table: "Courses");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Instructors_CourseId",
+                table: "Instructors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_InstructorId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "InstructorId",
+                table: "Courses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instructors_CourseId",
+                table: "Instructors",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructors_Courses_CourseId",
+                table: "Instructors",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructors_Courses_CourseId",
+                table: "Instructors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Instructors_CourseId",
+                table: "Instructors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InstructorId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Instructors_CourseId",
+                table: "Instructors",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_InstructorId",
+                table: "Courses",
+                column: "InstructorId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Instructors_InstructorId",
+                table: "Courses",
+                column: "InstructorId",
+                principalTable: "Instructors",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using LearningManagementSystem.Context;
+using LearningManagementSystem.Implementations.Repositories;
+using LearningManagementSystem.Implementations.Services;
+using LearningManagementSystem.Interfaces.Repositories;
+using LearningManagementSystem.Interfaces.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LearningApplicationContext>(options =>
+            options.UseMySQL(Configuration.GetConnectionString("ConnectionContext")));
+            services.AddControllersWithViews();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IDepartmentServices, DepartmentServices>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ICourseServices, CourseServices>();
+            services.AddScoped<IInstructorRepository, InstructorRepository>();
+            services.AddScoped<IInstructorServices, InstructorServices>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IStudentServices, StudentServices>();
+
+            services.AddSession();
+            services.AddAuthentication();
+            services.AddAuthorization();
+
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
